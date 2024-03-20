de025ca14ddef3a7a82231436f09c7afa8a2ab39
Thiene-L <qq1031828119@gmail.com>
2024-03-20 13:39:11 -0500

    Register and signin

--- src/screens/Registration.js
+++ src/screens/Registration.js
@@ -1,48 +1,79 @@
-import React from 'react'
-import { Link
- } from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+
 function Registration() {
-  return (
-    <div className='bg-[#e7e9f9] h-screen w-screen flex justify-center items-center'>
-      <div className='flex-column'>
-        <div className='font-bold mb-20 items-center w-80 text-center text-2xl text-[#ff5c60]'>
-          Registration
-        </div>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-        <div className='flex-column'>
-          <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Email</div>
-          
-          <input type='text' className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'></input>
-        </div>
+    const handleRegistration = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:8080/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({email: email, password: password}),
+            });
 
-        <div className='flex-column mt-10'>
-          <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Password</div>
-          
-          <input type='text' className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'></input>
-        </div>
+            if (response.ok) {
+                // If registration is successful, redirect to the login page
+                alert('Registration successful!')
+                navigate('/');
+            } else {
+                // If registration is not successful, show an alert
+                alert('Registration failed');
+            }
+        } catch (error) {
+            console.error('Registration error:', error);
+            alert('An error occurred during registration');
+        }
+    };
 
-        <div className='flex-column mt-10'>
-          <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Account Name</div>
-          
-          <input type='text' className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'></input>
-        </div>
+    return (
+        <div className='bg-[#e7e9f9] h-screen w-screen flex justify-center items-center'>
+            <div className='flex-column'>
+                <div className='font-bold mb-20 items-center w-80 text-center text-2xl text-[#ff5c60]'>
+                    Registration
+                </div>
 
-        <div className='my-10'>
-          <Link to = '/shorten'>
-          <button className='text-white rounded-lg bg-[#4e60ff] p-2 w-80 h-10'>Login</button>
-          </Link>
-          
-        </div>      
-          
-        <div>
-          <button className='text-white rounded-lg bg-red-500 p-2 w-80 h-10'>Login - Google</button>
+                <form onSubmit={handleRegistration}>
+                    <div className='flex-column'>
+                        <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Email</div>
+                        <input
+                            type='email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'
+                        ></input>
+                    </div>
+
+                    <div className='flex-column mt-10'>
+                        <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Password</div>
+                        <input
+                            type='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'
+                        ></input>
+                    </div>
+
+                    <div className='my-10'>
+                        <button type='submit' className='text-white rounded-lg bg-[#4e60ff] p-2 w-80 h-10'>Register
+                        </button>
+                    </div>
+                </form>
+
+                <div>
+                    <button className='text-white rounded-lg bg-red-500 p-2 w-80 h-10'>Login - Google</button>
+                </div>
+                <div className='flex justify-center font-semibold mt-1'>
+                    <Link to='/'> Back to Login</Link>
+                </div>
+            </div>
         </div>
-        <div className = 'flex justify-center font-semibold mt-1'>
-         <Link to = '/'> Back to Login</Link>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
-export default Registration
+export default Registration;
--- src/screens/Signin.js
+++ src/screens/Signin.js
@@ -1,43 +1,75 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 
 function Signin() {
-  return (
-    <div className='bg-[#e7e9f9] h-screen w-screen flex justify-center items-center'>
-      <div className='flex-column'>
-        <div className='font-bold mb-20 items-center w-80 text-center text-2xl'>
-          SignIn
-        </div>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-        <div className='flex-column'>
-          <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Email</div>
-          
-          <input type='text' className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'></input>
-        </div>
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const response = await fetch('http://localhost:8080/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email: email, password: password}),
+        });
 
-        <div className='flex-column mt-10'>
-          <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Password</div>
-          
-          <input type='text' className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'></input>
-        </div>
+        if (response.ok) {
+            // If login is successful, redirect to the home page
+            navigate('/home');
+        } else {
+            // If login is not successful, show an alert
+            alert('Invalid login credentials');
+        }
+    };
 
-        <div className='my-10'>
-          <Link to = '/shorten'>
-          <button className='text-white rounded-lg bg-[#4e60ff] p-2 w-80 h-10'>Login</button>
-          </Link>
-        </div>      
-          
-        <div className = 'my-10'>
-          <button className='text-white rounded-lg bg-red-500 p-2 w-80 h-10'>Login - Google</button>
-        </div>
+    return (
+        <div className='bg-[#e7e9f9] h-screen w-screen flex justify-center items-center'>
+            <div className='flex-column'>
+                <div className='font-bold mb-20 items-center w-80 text-center text-2xl'>
+                    SignIn
+                </div>
 
-        <div className = 'flex justify-center font-semibold mt-1'>
-         <Link to = '/registration'> Register </Link>
+                <form onSubmit={handleLogin}>
+                    <div className='flex-column'>
+                        <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Email</div>
+                        <input
+                            type='text'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'
+                        />
+                    </div>
+
+                    <div className='flex-column mt-10'>
+                        <div className='text-[#5f6bcb] mb-2 font-bold text-xl'>Password</div>
+                        <input
+                            type='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className='bg-[#e7e9f9] rounded-lg border-[#cfd0db] border-2 w-80 h-10'
+                        />
+                    </div>
+
+                    <div className='my-10'>
+                        <button type='submit' className='text-white rounded-lg bg-[#4e60ff] p-2 w-80 h-10'>
+                            Login
+                        </button>
+                    </div>
+                </form>
+
+                <div className='my-10'>
+                    <button className='text-white rounded-lg bg-red-500 p-2 w-80 h-10'>Login - Google</button>
+                </div>
+
+                <div className='flex justify-center font-semibold mt-1'>
+                    <Link to='/registration'>Register</Link>
+                </div>
+            </div>
         </div>
-        
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Signin
+export default Signin;
